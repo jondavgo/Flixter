c47137014e32de5d8d9a0c022f4c9541a98b66e5
Jonathan Gomez <jondavgo@fb.com>
2020-06-24 17:02:45 -0400

    Finished design for item_movie view, created movie adapter class to help with recycler view

---
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -0,0 +1,15 @@
+package com.example.flixter.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MovieAdapter {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
